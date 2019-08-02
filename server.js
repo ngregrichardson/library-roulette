753eeee9ed4571fd2112136db0ad60cf65ff7ac9
Glitch (libraryroulette) <none>
2019-08-02 02:54:19 +0000

    Updated for hosting switch

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ app.get('/', function (request, response) {
     response.sendFile(__dirname + '/index.html');
 });
 
-const listener = app.listen(process.env.PORT, function () {
-
+app.get("/css", function(request, response) {
+  response.sendFile(__dirname + "/index.css");
 });
+
+const listener = app.listen(process.env.PORT || 6000, function() {});
