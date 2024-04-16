65b0d4b8e49ac0f142aad31c728a71aef5ecab0d
Noah Richardson <ngregrichardson@gmail.com>
2024-04-16 14:33:31 -0400

    Add new sounds

--- src/configs/sounds.ts
+++ src/configs/sounds.ts
@@ -1,1 +1,1 @@
-export const SOUNDS = ["daddy", "fart", "horn", "lazer", "pingas", "pipe", "fnaf", "ahh"];
+export const SOUNDS = ["daddy", "fart", "horn", "lazer", "pingas", "pipe", "fnaf", "ahh", "huh", "boom", "weeaww", "tomscream", "peenatrick", "ifeelgood", "uwu", "shutup", "warioohmygod"];
